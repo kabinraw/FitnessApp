cd88524ecb1c27b032a26a1ee591598cd022e685
kabindra karki <kabindra.2k15@gmail.com>
2024-09-30 23:23:37 +0545

    initial commit

---
+++ FitnessApp/FitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppApp.swift
+//  FitnessApp
+//
+//  Created by Kabindra on 9/30/24.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
